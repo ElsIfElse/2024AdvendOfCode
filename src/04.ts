2b361d356022ca9e7444e113886f09662e41076b
Sapimizera <szabolcs.sapi@gmail.com>
2024-12-11 15:59:26 +0100

    created path validation logic based on if there is enough space for the word to fit

--- src/04.ts
+++ src/04.ts
@@ -34,13 +34,17 @@ const makeStringArrayInto2dArray = function(stringArrayInput:string[]){
 const lookForX = function(stringArray2dInput:string[][]){
     for(let i = 0; i < stringArray2dInput.length;i++){
         for(let j = 0;j < stringArray2dInput[i].length;j++){
- 
-            console.log(lookWhereIsSpace(i,j,stringArray2dInput[i].length,stringArray2dInput.length))
+
+            // Check if iterated element is X and if it is then send it to check if it can move around
+            if(stringArray2dInput[i][j] == "X"){ 
+                lookWhereIsSpace(i,j,stringArray2dInput[i].length,stringArray2dInput.length)
+            }
+
         }
     }
 }
 
-// Checking wwich ones can be valid so the word "XMAS" can possibly fit towards a certain direction
+
 const lookWhereIsSpace = function(y:number,x:number,xLength:number,yLength:number){
 
     // if x <= 2 then it cannot look to the left.
@@ -48,10 +52,75 @@ const lookWhereIsSpace = function(y:number,x:number,xLength:number,yLength:numbe
     // if y <= 2  it cannot look upwards
     // if y > array.length-3  it cannot look downwards
 
-    console.log(y,x)
+    if(canGoLeft(x)){
+        
+    } 
+
+    const up:boolean = canGoUpwards(y)
+    const down:boolean = canGoDownwards(y,yLength);
+
+
+    const right:boolean = canGoRight(x,xLength)
+
+    const upLeft:boolean = canGoLeftUp(left,up);
+    const upRigh:boolean = canGoRightUp(right,up);
+
+    const downLeft:boolean = canGoLeftDown(left,down);
+    const downRight:boolean = canGoRightDown(right,down);
+
+    console.log(downLeft)
+
 }
 
-
-const lookLeft = function(){
+// Checking which ones can be valid so the word "XMAS" can possibly fit towards a certain direction
+const canGoLeft = function(x:number){
+    if(x<=2){
+        return false;
+    }
+    return true;
+}
+const canGoRight = function(x:number,xLength:number){
+    if(x>xLength-4){
+        return false;
+    }
+    return true;
+}
+const canGoUpwards = function(y:number){
+    if(y<=2){
+        return false;
+    }
+    return true;
+}
+const canGoDownwards = function(y:number,yLength:number){
+    if(y>yLength-4){
+        return false;
+    }
+    return true;
+}
+const canGoLeftUp = function(left:boolean,up:boolean){
+    if(left && up){
+        return true
+    }
+    return false;
+}
+const canGoRightUp = function(right:boolean,up:boolean){
+    if(right && up){
+        return true
+    }
+    return false;
+}
+const canGoLeftDown = function(left:boolean,down:boolean){
+    if(left && down){
+        return true
+    }
+    return false;
+}
+const canGoRightDown = function(right:boolean,down:boolean){
+    if(right && down){
+        return true
+    }
+    return false;
+}
+const goingLeft = function(){
 
 }
